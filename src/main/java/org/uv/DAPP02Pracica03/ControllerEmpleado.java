c1248ae92cdb1fb004163ea84d7ee08011eedcee
josemmanuel <emmanuelriv15@gmail.com>
2024-04-26 15:41:32 -0600

    add modified

--- src/main/java/org/uv/DAPP02Pracica03/ControllerEmpleado.java
+++ src/main/java/org/uv/DAPP02Pracica03/ControllerEmpleado.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,18 +46,38 @@ public class ControllerEmpleado {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
-        return null;
+    public ResponseEntity<Empleado> put(@PathVariable Long id, @RequestBody Empleado empleado) {
+        Optional<Empleado> dataEmpleado = repositoryEmpleado.findById(id);
+        if (dataEmpleado.isPresent()) {
+            Empleado _empleado = dataEmpleado.get();
+            _empleado.setNombre(empleado.getNombre());
+            _empleado.setDireccion(empleado.getDireccion());
+            _empleado.setTelefono(empleado.getTelefono());
+            repositoryEmpleado.save(_empleado);
+            return ResponseEntity.ok().body(_empleado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<?> post(@RequestBody Object input) {
-        return null;
+    public ResponseEntity<Empleado> post(@RequestBody Empleado empleado) {
+        try {
+            Empleado savedEmpleado = repositoryEmpleado.save(empleado);
+            return ResponseEntity.ok().body(savedEmpleado);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable String id) {
-        return null;
+    public ResponseEntity<Empleado> delete(@PathVariable Long id) {
+        try {
+            repositoryEmpleado.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
